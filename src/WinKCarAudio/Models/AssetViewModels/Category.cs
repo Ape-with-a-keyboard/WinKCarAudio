dd3fe84dbb7c3277522e8898bf3b50a0249d0ced
Ape-with-a-keyboard <vinsonn2002@gmail.com>
2017-06-21 14:46:27 -0600

    added in Category model

---
+++ src/WinKCarAudio/Models/AssetViewModels/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WinKCarAudio.Models.AssetViewModels
+{
+    public class Category
+    {
+        /// <summary>
+        /// This holds asset category.
+        /// </summary>
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        // Navigation properties for many-to-many relationship between
+        // Asset and Category.
+        // One category can have many assets.
+        public ICollection<AssetCategory> AssetCategories { get; set; }
+
+    }
+}
