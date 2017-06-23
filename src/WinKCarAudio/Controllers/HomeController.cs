144117f89f3a3759630b4f60ba191a3159f68596
Ape-with-a-keyboard <vinsonn2002@gmail.com>
2017-06-23 09:56:27 -0600

    working on home display controller

--- src/WinKCarAudio/Controllers/HomeController.cs
+++ src/WinKCarAudio/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WinKCarAudio.Data;
+using WinKCarAudio.Models.AssetViewModels;
 
 namespace WinKCarAudio.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         public IActionResult Index()
         {
-            return View();
+            var viewModel = new AssetIndexData();
+            viewModel.Assets = _context.Asset
+                .Include(a => a.ImageGallery)
+                    .ThenInclude(i => i.Images)
+                .Where(m => m.featuredItem == true);
+
+            return View(viewModel);
         }
 
         public IActionResult About()
